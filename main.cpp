3a62fba0ab89a18b863acba191c4e22cdf9fb49d
Stephen Schwab <zenight.schwab@gmail.com>
2018-09-21 08:56:29 -0400

    Check this out, it's a commit!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <SDL.h>
 
-int main()
+int main(int argc, char **argv)
 {
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		std::cout << "There was an error initializing SDL! SDL error: " << SDL_GetError() << std::endl;
+		return -1;
+	}
     std::cout << "Hello, world!" << std::endl;
-    return 0;
+    return EXIT_SUCCESS;
 }
